e5e66c2dfeb17adc3fdf6f805f6b86b6a6af2876
LukeVilla <lukevlobos@gmail.com>
2025-05-16 18:06:04 -0700

    Setup screen now works

--- pymail.py
+++ pymail.py
@@ -1,7 +1,8 @@
 from textual.app import App
-from textual import log
+from textual import log, events, on, work
 from textual.screen import Screen
-from textual.widgets import Footer, Label, TextArea, Welcome, ListItem, Button, ListView, Placeholder
+from textual.containers import Vertical
+from textual.widgets import Footer, Label, Input, TextArea, Welcome, ListItem, Button, ListView, Placeholder
 import configparser, os, sys, string, argparse
 from imap_tools import MailBox
 from render_html import render_in_browser as render
@@ -10,6 +11,27 @@ try:
 except ImportError:
     print("Warning: getpass_asterisk not found. Defaulting to getpass.")
     from getpass import getpass
+
+class Setup(Screen):
+    BINDINGS = [("escape", "exit_screen", "Save"),
+                ("ctrl+s", "open_settings", "Settings")]
+    def compose(self):
+        self.refresh_bindings()
+        yield Label("Address:")
+        yield Input("", id="address")
+        yield Label("Server:")
+        yield Input("", id="server")
+        yield Footer()
+    def action_exit_screen(self):
+        self.refresh_bindings()
+        addr = self.query_one("#address")
+        serv = self.query_one("#server")
+        self.dismiss({"addr": addr.value, "serv": serv.value})
+    def check_action(self, action, parameters):
+        self.refresh_bindings()
+        if action == "open_settings":
+            return None
+        return True
 
 def setup(conf):
     addr = input("Enter email address: ")
@@ -54,6 +76,15 @@ def read_conf(field):
     opts["server"] = conf["account"]["serv"]
     return opts[field]
 
+def write_conf(field, new, category="account"):
+    conf = configparser.ConfigParser()
+    opts = {}
+    if not os.path.exists("pymail.ini"):
+        setup(conf)
+    conf.read("pymail.ini")
+    conf[category][field] = new
+    conf.write(conf)
+
 options = do_args()
 if options["server"]:
     serv = options["server"]
@@ -72,10 +103,25 @@ with MailBox(serv).login(addr, password) as mailbox:
     mail = list(mailbox.fetch())
 class PyMail(App):
     CSS_PATH = "pymail.tcss"
+    SCREENS = {"setup": Setup}
+    BINDINGS = [("ctrl+s", "open_settings", "Settings"),
+                ("ctrl+q", "exit", "Exit")]
+    
+    def write_settings(self, settings):
+        self.refresh_bindings()
+        for key, val in settings.items():
+            write_conf(key, val)
+        log(settings)
+    def action_open_settings(self):
+        self.refresh_bindings()
+        self.push_screen(Setup(), self.write_settings)
+        self.refresh_bindings()
     def action_html(self):
         # self.sanitized_html = sanitize(self.current_html)
         # self.log(self.current_html)
         render(self.current_html, encoding="utf8")
+    def action_exit(self):
+        self.exit()
     def update_label_if_exists(self, widget, new_text, new_id = None):
         if self.query(widget):
             dispwidget = self.query_one(widget)
@@ -109,20 +155,19 @@ class PyMail(App):
         pass
     def compose(self):
         subs = []
+        # bottom = [Label("Press Ctrl-Q to exit.", id="exit"), Footer()]
         for msg in mail:
             subs.append(ListItem(Label(msg.subject)))
         # print(subs)
         subs = list(reversed(subs))
         self.emailsnum = len(subs)
         yield ListView(*subs, id="emails")
-        yield Label("Press Ctrl-Q to exit.", id="exit")
-    def on_button_pressed(self):
-        self.exit()
+        # yield Vertical(*bottom, id="bottom")
+        # yield Label("Press Ctrl-Q to exit.", id="exit")
+        yield Footer()
     def on_list_view_selected(self, event):
         view = self.query_one(ListView)
         self.log(f"Email selected: {view.index}")
         self.handle_select((self.emailsnum - view.index) - 1)
-class Setup(Screen):
-    pass
 app = PyMail()
 app.run()
